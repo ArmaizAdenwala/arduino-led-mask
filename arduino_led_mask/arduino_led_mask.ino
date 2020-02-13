16d98bb73c733dc17c50204619a205ae95642622
Armaiz Adenwala <armaiz.adenwala@gmail.com>
2020-02-12 22:53:53 -0800

    add effects

--- arduino_led_mask/arduino_led_mask.ino
+++ arduino_led_mask/arduino_led_mask.ino
@@ -6,7 +6,7 @@
 #define NUM_FIRST_ROW 26
 #define LED_TYPE WS2812B
 #define COLOR_ORDER RGB
-#define BRIGHTNESS 150
+#define BRIGHTNESS 160
 
 CRGB leds[NUM_LEDS];
 
@@ -25,11 +25,70 @@ void setup()
 // Examples of designs you can loop through
 void loop()
 {
+  // fillColor(10, 160, 103);
+  // fillColor(60, 10, 230);
+  // fillColor(100, 20, 250);
+  // fillColor(10, 200, 163);
+  // fillColor(10, 170, 223);
+  // FastLED.show();
+  // FastLED.delay(500);
+  // fillColor(80, 10, 180);
+  // FastLED.show();
+  // FastLED.delay(500);
   // fadePinkBlue();
-  // rainbowFade();
+  fadeToColor(0, 0, 200, 0, 40, 80, 150, 1);
+  delay(10);
+  fadeToColor(0, 40, 80, 0, 0, 200, 150, 1);
+  delay(10);
+  fadeToColor(0, 0, 200, 0, 40, 80, 150, 1);
+  delay(10);
+  rainbowFade();
   circle();
+  circle();
+  circle();
+  circle();
+  circleInverse();
+  circleInverse();
+  circleInverse();
+  circleInverse();
   // FastLED.setMaxPowerInVoltsAndMilliamps(3, 500);
   // FastLED.delay(200);
+}
+
+void circleInverse()
+{
+  int pattern[NUM_LEDS] = {
+    0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 1, 1, 1, 0, 4, 3, 2, 1, 0, 4, 3, 2, 1, 0,
+     1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 2, 2, 1, 0, 4, 3, 2, 1, 0, 4, 3, 2, 1,
+      2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 3, 2, 1, 0, 4, 3, 2, 1, 0, 4, 3, 2,
+       3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 3, 2, 1, 0, 4, 3, 2, 1, 0, 4, 3,
+        3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 3, 2, 1, 0, 4, 3, 2, 1, 0, 4, 3,
+         3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 2, 2, 1, 0, 4, 3, 2, 1, 0, 4, 3,
+          3, 4, 0, 1, 2, 3, 4, 0, 1, 1, 1, 1, 0, 4, 3, 2, 1, 0, 4, 3
+  };
+  int rgbColors[5][3] = {
+      {5, 40, 190},
+      {5, 40, 190},
+      {5, 40, 190},
+      {80, 5, 190},
+      {80, 5, 190}
+  };
+
+  for (int x = 4; x >= 0; x--) {
+    for (int z = 0; z < 6; z++) {
+      for (int i = 0; i < NUM_LEDS; i++) {
+        int color = (pattern[i] + x) % 5;
+        int nextColor = (color + 1) % 5;
+        float r = getColorFade(rgbColors[nextColor][0], rgbColors[color][0], z, 6, i);
+        float g = getColorFade(rgbColors[nextColor][1], rgbColors[color][1], z, 6, i);
+        float b = getColorFade(rgbColors[nextColor][2], rgbColors[color][2], z, 6, i);
+        leds[i] = CRGB(r, g, b);
+      }
+      FastLED.show();
+      FastLED.setMaxPowerInVoltsAndMilliamps(5, 750);
+      FastLED.delay(1);
+    }
+  }
 }
 
 void circle()
@@ -43,25 +102,51 @@ void circle()
          3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 2, 2, 1, 0, 4, 3, 2, 1, 0, 4, 3,
           3, 4, 0, 1, 2, 3, 4, 0, 1, 1, 1, 1, 0, 4, 3, 2, 1, 0, 4, 3
   };
+  // int rgbColors[5][3] = {
+  //     {160, 10, 103},
+  //     {5, 5, 230},
+  //     {5, 5, 230},
+  //     {5, 5, 230},
+  //     {5, 5, 230}
+  // }
+
   int rgbColors[5][3] = {
-      {10, 50, 90},
-      {40, 20, 40},
-      {170, 30, 60},
-      {190, 70, 180},
-      {10, 150, 162},
+      {120, 10, 143},
+      {100, 30, 233},
+      {5, 5, 230},
+      {5, 5, 230},
+      {5, 5, 230},
   };
-  for (int z = 0; z < 5; z++) {
-    for (int i = 0; i < NUM_LEDS; i++) {
-      int color = (pattern[i] + z) % 5;
-      leds[i] = CRGB(rgbColors[color][0], rgbColors[color][1], rgbColors[color][2]);
+  for (int x = 0; x < 5; x++) {
+    for (int z = 0; z < 4; z++) {
+      for (int i = 0; i < NUM_LEDS; i++) {
+        int color = (pattern[i] + x) % 5;
+        int nextColor = (color + 1) % 5;
+        float r = getColorFade(rgbColors[color][0], rgbColors[nextColor][0], z, 4, i);
+        float g = getColorFade(rgbColors[color][1], rgbColors[nextColor][1], z, 4, i);
+        float b = getColorFade(rgbColors[color][2], rgbColors[nextColor][2], z, 4, i);
+        leds[i] = CRGB(r, g, b);
+      }
+      FastLED.show();
+      FastLED.setMaxPowerInVoltsAndMilliamps(5, 750);
+      FastLED.delay(1);
     }
-    delay(200);
-    FastLED.show();
   }
 }
 
-void getIndex() {
-
+float getColorFade(int a, int b, int index, int range, int i)
+{
+  if (a == b)
+  {
+    return a;
+  }
+  int dif = abs(a - b);
+  float change = (float)dif / range * (index + 1);
+  if (a > b)
+  {
+    return a - change;
+  }
+  return a + change;
 }
 
 void rainbowFade()
